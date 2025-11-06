e588caa95373a2ae5f1f70304b1c42d84bc4dfc0
Cusican33 <Yarielp12@nycstudents.net>
2025-11-06 12:35:17 -0500

    work

---
+++ src/APCalendar.java
@@ -0,0 +1,31 @@
+public class APCalendar
+{
+    public static boolean isLeapYear(int year)
+    {
+        boolean x = year % 4 == 0;
+        boolean y = year % 100 == 0;
+        boolean z = year % 400 == 0;
+        return (x && !y) || z;
+    }
+    public static int numberOfLeapYears(int year1, int year2)
+    {
+        int years = 0;
+        for (int i = year1; i <= year2; i++)
+        {
+            if (isLeapYear(i)) years++;
+        }
+        return years;
+    }
+    private static int firstDayOfYear(int year)
+    {
+        return 0;
+    }
+    private static int dayOfYear(int month, int day, int year)
+    {
+        return 0;
+    }
+    public static int dayOfWeek(int month, int day, int year)
+    {
+        return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        APCalendar c = new APCalendar();
+        System.out.println(c.numberOfLeapYears(1200, 2000));
+    }
+}
