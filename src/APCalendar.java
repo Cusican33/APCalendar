5cdf678704bbba0546f1052255f2b71a00241185
Cusican33 <Yarielp12@nycstudents.net>
2025-11-12 08:48:29 -0500

    Update APCalendar.java

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,6 +1,6 @@
 public class APCalendar
 {
-    public static boolean isLeapYear(int year)
+    private static boolean isLeapYear(int year)
     {
         boolean x = year % 4 == 0;
         boolean y = year % 100 == 0;
@@ -19,14 +19,29 @@ public class APCalendar
     }
     public static int firstDayOfYear(int year)
     {
-        return (year + numberOfLeapYears(0, year)) % 7;
+        return (year + numberOfLeapYears(1, year)) % 7;
     }
     private static int dayOfYear(int month, int day, int year)
     {
-        return 0;
+        int days = 0;
+        if (month > 1) days += 31;
+        if (month  > 2) days += 28;
+        if (isLeapYear(year)) days ++;
+        if (month > 3) days += 31;
+        if (month > 4) days += 30;
+        if (month > 5) days += 31;
+        if (month > 6) days += 30;
+        if (month > 7) days += 31;
+        if (month > 8) days += 31;
+        if (month > 9) days += 30;
+        if (month > 10) days += 31;
+        if (month > 11) days += 30;
+        days += day;
+        return days;
     }
     public static int dayOfWeek(int month, int day, int year)
     {
-        return 0;
+        int days = dayOfYear(month, day, year) & 7;
+        return firstDayOfYear(year) + days - 1;
     }
 }
