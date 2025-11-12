3888064b1e3d8f669d0bc7fdac1b216c307fb8d0
Cusican33 <Yarielp12@nycstudents.net>
2025-11-12 08:48:15 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         APCalendar c = new APCalendar();
-        System.out.println(c.firstDayOfYear(2019));
-        System.out.println(c.numberOfLeapYears(0,0));
-        System.out.println(c.isLeapYear(0));
+        System.out.println(c.dayOfWeek(1, 5, 2019));
+        System.out.println(c.dayOfWeek(1, 10, 2019));
     }
 }
